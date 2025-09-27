d2cab24ff004e4964dfe8bcb8254459bd9f7c7bc
Gambito404 <gabrielberriosmendoza@gmail.com>
2025-09-26 23:57:04 -0400

    Add files via upload

---
+++ scripts/productos-sonido.js
@@ -0,0 +1,30 @@
+const productosSonido = {
+    "sonido-1": { 
+        nombre: "Sonido Básico", 
+        descripcion: "Ideal para fiestas pequeñas y reuniones.", 
+        precio: 1500, 
+        imagen: "imagenes/sonido_1.jpg", 
+        type: 'sonido',
+        componentes: [
+            '2 Altavoces Pasivos de 15"',
+            '1 Mezcladora de 4 canales',
+            '2 Trípodes para altavoces',
+            'Cableado necesario'
+        ] 
+    },
+    "sonido-2": { 
+        nombre: "Sonido Intermedio", 
+        descripcion: "Perfecto para bodas y eventos corporativos.", 
+        precio: 2800, 
+        imagen: "imagenes/sonido_2.jpg", 
+        type: 'sonido',
+        componentes: [
+            '2 Altavoces Activos de 15"',
+            '1 Subwoofer Activo de 18"',
+            '1 Mezcladora de 8 canales',
+            '1 Micrófono inalámbrico',
+            'Cableado necesario'
+        ]
+    },
+    "sonido-3": { nombre: "Set de DJ", descripcion: "Consola profesional de DJ con monitores de alta fidelidad.", precio: 3500, imagen: "imagenes/sonido_3.jpg", type: 'sonido', componentes: ['1 Consola DJ Pioneer', '2 Monitores de estudio', '1 Audífonos profesionales', 'Cableado de alta calidad'] },
+};
---
+++ scripts/productos-tarimas.js
@@ -0,0 +1,18 @@
+const productosTarimas = {
+    "tarima-1": { 
+        nombre: "Tarima Pequeña (4x3m)", 
+        descripcion: "Compuesta por 6 módulos. Ideal para presentaciones o DJs.", 
+        precio: 2000, 
+        imagen: "imagenes/tarima_1.jpg",
+        type: 'tarima',
+        alturas: ['80cm', '1.30m', '1.80m']
+    },
+    "tarima-2": { 
+        nombre: "Tarima Mediana (6x4m)", 
+        descripcion: "Compuesta por 12 módulos. Espacio suficiente para una banda pequeña.", 
+        precio: 4000, 
+        imagen: "imagenes/tarima_2.jpg",
+        type: 'tarima',
+        alturas: ['80cm', '1.30m', '1.80m']
+    }
+};
